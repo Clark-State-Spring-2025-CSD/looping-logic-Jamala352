83a70b846178cd8a92743732913ae4a7e7776126
Jamala352 <ashlej2513@clarkstate.edu>
2025-03-03 14:02:53 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -13,3 +13,16 @@
 #13
 #14
 #FizzBuzz
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Call the function with n = 25
+fizzbuzz(25)
